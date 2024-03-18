4638b2bac213c2f7c34eb40629491fc51a7f8fbd
karnveer <karnveersharma1994@gmail.com>
2024-03-18 18:32:57 +0530

    Added test case for two digits input separated by comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe StringCalculator do
   it "should return the same number when a single digit string is given" do
     expect(string_calculator.add("1")).to eq(1)
   end
+
+  it "should return the sum of digits when two digits are given separated by comma" do
+    expect(string_calculator.add("1,5")).to eq(6)
+  end
 end
