3e7cd37b2595b9942c897f15613247ef9cf62a2a
karnveer <karnveersharma1994@gmail.com>
2024-03-18 18:38:57 +0530

    Updated add method to handle invalid input when new line at the end of input is given

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
     return input.to_i if contains_a_single_number(input)
+    return "Invalid Input" if input.end_with?("\n")
     sum_of_numbers(input)
   end
 
