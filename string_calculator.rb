b61f2cc0dc1ff8e8fddbab10b5e84d6a18a6d7f3
karnveer <karnveersharma1994@gmail.com>
2024-03-18 18:34:45 +0530

    Updated add method to handle two or more digits

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,9 +2,15 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
     return input.to_i if contains_a_single_number(input)
+    sum_of_numbers(input)
   end
 
   def contains_a_single_number(input)
     input.length == 1 && (input =~ /[0-9]/) == 0 && input.to_i  >= 0
+  end
+
+  def sum_of_numbers(input)
+    numbers = input.split(',').map(&:to_i)
+    numbers.sum
   end
 end
