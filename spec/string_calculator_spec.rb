115a29ebb5974cc791ae78c382386935955511ce
karnveer <karnveersharma1994@gmail.com>
2024-03-18 18:30:56 +0530

    Added test case for single digit input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe StringCalculator do
   it "should return 0 when an empty string is given" do
     expect(string_calculator.add("")).to eq(0)
   end
+
+  it "should return the same number when a single digit string is given" do
+    expect(string_calculator.add("1")).to eq(1)
+  end
 end
