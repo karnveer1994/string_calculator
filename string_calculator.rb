3347c9824364ba6fa4c456c5effaeedf9aad7f9c
karnveer <karnveersharma1994@gmail.com>
2024-03-18 18:31:48 +0530

    implemented second case

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,10 @@
 class StringCalculator
   def add(input)
     return 0 if input.empty?
+    return input.to_i if contains_a_single_number(input)
+  end
+
+  def contains_a_single_number(input)
+    input.length == 1 && (input =~ /[0-9]/) == 0 && input.to_i  >= 0
   end
 end
