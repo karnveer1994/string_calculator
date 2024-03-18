9763231e77b3598bffc9f0b72610afdf352b4224
karnveer <karnveersharma1994@gmail.com>
2024-03-18 18:41:20 +0530

    Updated add method to handle input contains delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -10,8 +10,17 @@ class StringCalculator
     input.length == 1 && (input =~ /[0-9]/) == 0 && input.to_i  >= 0
   end
 
+  def contains_a_delimiter(input)
+    input.start_with?("//")
+  end
+
   def sum_of_numbers(input)
-    numbers = input.split(/[,\n]/).map(&:to_i)
+    if contains_a_delimiter(input)
+      delimiter = input.split("//").last[0]
+      numbers = input.split("\n").last.split(delimiter).map(&:to_i)
+    else
+      numbers = input.split(/[,\n]/).map(&:to_i)
+    end
     numbers.sum
   end
 end
